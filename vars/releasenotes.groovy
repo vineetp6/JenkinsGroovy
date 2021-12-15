fa05e5c63f893dc822b2a29da9829cd420bced34
bigquery272 <bigquery272@gmail.com>
2021-12-14 18:43:34 -0800

    new file added2

--- vars/releasenotes.groovy
+++ vars/releasenotes.groovy
@@ -1,10 +1,12 @@
-import java.io.*;
-import groovy.io.*;
+import groovy.io.FileType;
+import java.io.File;
+import java.text.SimpleDateFormat
 
 @NonCPS
 def call(Map config=[:]){
-def dir = new File(pwd());
-new File(dir.path + '/releasenotes.txt').withWriter('utf-8') 
+	def dir = new File(pwd());
+    
+	new File(dir.path + '/releasenotes.txt').withWriter('utf-8') 
 	{ 
 		writer -> 
 	    dir.eachFileRecurse(FileType.ANY){ file ->
@@ -17,4 +19,12 @@ new File(dir.path + '/releasenotes.txt').withWriter('utf-8')
 		}
 		} 
 	}
+    
+	def date = new Date()
+	def sdf = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss")
+	echo "Date and Time IS: " + sdf.format(date)
+    
+    if (config.changes != "false"){
+    	echo "changes";
+    }
 }
